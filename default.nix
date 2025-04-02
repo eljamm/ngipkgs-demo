0409df3df344b7bf53b562ede61a1c21912d5f30
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-02 19:31:19 +0200

    disable kvm
    now the vm works inside the container and the service can be accessed
    from the host!

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 {
   ngipkgs ?
     import
-      (fetchTarball "https://github.com/eljamm/ngipkgs/tarball/init-project-demos/8eb7f038fd62fd6490e017d78e6a30e7b64a13fa")
+      (fetchTarball "https://github.com/eljamm/ngipkgs/tarball/init-project-demos-bak/dd47924912b7")
       { },
 }:
 ngipkgs.demo {
