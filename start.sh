5d25f6ece35b8e0f2e5e180bf971d62a07556393
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-03 23:47:47 +0200

    enabel flakes; cache store with volumes

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ podman run \
     --rm -it \
     --mount type=bind,source=./default.nix,target=/ngi-demo/default.nix \
     --mount type=bind,source=./nix.conf,target=/etc/nix/nix.conf \
+    --mount type=volume,source=cache,target=/cache \
     localhost/nix-ngi:latest \
     /bin/sh
