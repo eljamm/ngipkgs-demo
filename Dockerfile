aab094ea37e494d7237ffa4b93b107c822ff2493
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-04 02:07:33 +0200

    cache stuff

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
 FROM nixos/nix
+
+COPY nix.conf /etc/nix/nix.conf
+
+WORKDIR /ngi-demo
+COPY default.nix .
+
 CMD [ "/bin/sh" ]
--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ podman run \
     --rm -it \
     --mount type=bind,source=./default.nix,target=/ngi-demo/default.nix \
     --mount type=bind,source=./nix.conf,target=/etc/nix/nix.conf \
-    --mount type=volume,source=cache,target=/cache \
-    localhost/nix-ngi:latest \
-    /bin/sh
+    --mount type=volume,source=nix-cache,target=/nix-cache \
+    --env NIX_STORE_DIR=/nix-cache \
+    localhost/nix-ngi:latest
